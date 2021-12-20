0dc14f32be65c5857e5d7f59044faf41dce3effc
Theodore Ando <theodoreando99@gmail.com>
2021-12-20 16:49:52 -0600

    work on plots

--- areyoutheone.py
+++ areyoutheone.py
@@ -1,6 +1,11 @@
+from math import sqrt, ceil
 from typing import List
 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 import pandas as pd
+from matplotlib.patches import Rectangle
 from ortools.sat.python.cp_model import CpModel, CpSolver
 from pandas.io.formats.style import Styler
 
@@ -16,15 +21,16 @@ class AreYouTheOne:
             raise ValueError("Must have positive number of people")
         self.men = men
         self.women = women
+        self.truth_booths = {}
         self.model = CpModel()
         self.solver = CpSolver()
         self.solver.parameters.enumerate_all_solutions = True
 
         # x_(m,w) is 1 if the couple (m,w) is a match, 0 otherwise
         self.x = {
-            (m, w): self.model.NewBoolVar(f"Match({m}, {w})")
-            for m in men
-            for w in women
+                (m, w): self.model.NewBoolVar(f"Match({m}, {w})")
+                for m in men
+                for w in women
         }
         self.counter = MatchSolutionCounter(self.x)
         self.scorer = SolutionScorerCallback(self.x)
@@ -41,6 +47,7 @@ class AreYouTheOne:
         self.model.Add(n_perfect == sum(self.x[mw] for mw in matches))
 
     def add_truth_booth(self, m: str, w: str, perfect: bool):
+        self.truth_booths[(self.men.index(m), self.women.index(w))] = int(perfect)
         self.model.Add(self.x[(m, w)] == int(perfect))
 
     def solve(self, solution_callback=None, **kwargs) -> MatchSolutionCounter:
@@ -87,20 +94,65 @@ class AreYouTheOne:
         """Pick the matching that provides the min expected number of remaining solutions"""
         return self.scorer.evaluate_matching(matching)
 
-    def display(self) -> Styler:
+    def expected_lights(self, matching: Matching_t) -> float:
+        """Compute the expected number of lights that this matching will get given current model state"""
+        return self.scorer.expected_lights(matching)
+
+    def display(self):
         counts = self.counter.counts()
         df = pd.DataFrame(
             data=[[counts[(m, w)] for w in self.women] for m in self.men],
             index=self.men,
             columns=self.women
         )
-        return df.style.background_gradient(axis=None) \
-            .set_caption(f"Number of Matches in {self.counter.n_solutions()} Solutions") \
-            .set_table_styles([{
-                'selector': 'caption',
-                'props': [
-                    ('color', 'black'),
-                    ('font-size', '16pt'),
-                    ('caption-side', 'center')
-                ]
-            }])
+
+        mask = np.zeros((len(self.men), len(self.women)))
+        for (i, j), b in self.truth_booths.items():
+            mask[i, j] = b
+        f, ax = plt.subplots(figsize=(9, 6))
+        ax.set_title(f"Number of Matches in {self.counter.n_solutions()} Solutions")
+        sns.heatmap(df, annot=True, ax=ax, fmt='d', cmap="Blues", mask=mask)
+
+        # apply green / red fill in boxes where we've gotten a TB
+        for (i, j), b in self.truth_booths.items():
+            color = 'green' if b else 'red'
+            # note that Rectangle loc is (x,y) not (row,col) hence we flip i,j
+            ax.add_patch(Rectangle((j, i), 1, 1, color=color, fill=True, lw=0, alpha=0.5))
+
+        # apply green / red outline in boxes where we've inferred a sure outcome
+        loc_nonmatches = np.where(df == 0)
+        loc_matches = np.where(df == self.counter.n_solutions())
+        tb_match_rows = {i for (i, j), b in self.truth_booths.items() if b == 1}
+        tb_match_cols = {j for (i, j), b in self.truth_booths.items() if b == 1}
+        inferred_nonmatches = {
+                (i, j)
+                for (i, j) in zip(*loc_nonmatches)
+                if (i not in tb_match_rows) and (j not in tb_match_cols) and ((i, j) not in self.truth_booths)
+        }
+        for (i, j) in inferred_nonmatches:
+            # note that Rectangle loc is (x,y) not (row,col) hence we flip i,j
+            ax.add_patch(Rectangle((j, i), 1, 1, color='red', fill=False, lw=1, alpha=0.5))
+        for (i, j) in zip(*loc_matches):
+            if (i, j) not in self.truth_booths:
+                # note that Rectangle loc is (x,y) not (row,col) hence we flip i,j
+                ax.add_patch(Rectangle((j, i), 1, 1, color='green', fill=False, lw=1))
+
+    def display_distributions(self, matching: Matching_t):
+        expected_n_sols = self.evaluate_matching(matching)
+        expected_n_lights = self.expected_lights(matching)
+
+        fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(12, 6))
+
+        # plot the distribution of expected number of solutions remaining, along with their choice
+        ax0.set_title('Distribution of E[# of solutions]')
+        ax0.set_ylabel('frequency')
+        ax0.set_xlabel(r'$\mathbb{E}$[# of solutions]', )
+        sns.histplot(self.scorer.scores(), ax=ax0)
+        ax0.axvline(expected_n_sols, color='r')
+
+        # plot the distribution of expected number of lights, along with their choice
+        ax1.set_title('Distribution of E[\# of lights]')
+        ax1.set_ylabel('frequency')
+        ax1.set_xlabel('E[# of lights]')
+        sns.histplot(self.scorer.lights(), ax=ax1)
+        ax1.axvline(expected_n_lights, color='r')
--- solutioncallbacks.py
+++ solutioncallbacks.py
@@ -3,20 +3,32 @@ from typing import List, Dict
 import numpy as np
 from ortools.sat.python.cp_model import CpSolverSolutionCallback
 
-from utils import Couple_t
+from utils import Couple_t, Matching_t
 
 
-class CompositeSolutionCallback(CpSolverSolutionCallback):
-    def __init__(self, callbacks: List[CpSolverSolutionCallback]):
+class MySolutionCallback(CpSolverSolutionCallback):
+    solver = None
+
+    def set_solver(self, solver):
+        self.solver = solver
+
+
+class CompositeSolutionCallback(MySolutionCallback):
+    def __init__(self, callbacks: List[MySolutionCallback]):
         super().__init__()
         self.callbacks = callbacks
+        for callback in self.callbacks:
+            callback.set_solver(self)
 
     def on_solution_callback(self):
-        for callback in self.callbacks:
-            callback.OnSolutionCallback()
+        try:
+            for callback in self.callbacks:
+                callback.OnSolutionCallback()
+        except Exception as e:
+            print("composite:", e)
 
 
-class MatchSolutionCounter(CpSolverSolutionCallback):
+class MatchSolutionCounter(MySolutionCallback):
     def __init__(self, variables, print_every=100):
         super().__init__()
         self._variables = variables
@@ -29,7 +41,7 @@ class MatchSolutionCounter(CpSolverSolutionCallback):
         if self._total_solns % self._print_every == 0:
             print(self._total_solns)
         for mw, x_mw in self._variables.items():
-            self._soln_cnts[mw] += self.Value(x_mw)
+            self._soln_cnts[mw] += self.solver.Value(x_mw)
 
     def n_solutions(self):
         return self._total_solns
@@ -38,7 +50,7 @@ class MatchSolutionCounter(CpSolverSolutionCallback):
         return self._soln_cnts
 
 
-class SolutionScorerCallback(CpSolverSolutionCallback):
+class SolutionScorerCallback(MySolutionCallback):
     def __init__(self, variables: Dict):
         super().__init__()
         self.variable_map = variables
@@ -46,8 +58,12 @@ class SolutionScorerCallback(CpSolverSolutionCallback):
         self.variables = [self.variable_map[mw] for mw in self.keys]
         self.solutions = []
 
+        self._has_comp_distr = False
+        self._expected_n_sols = []
+        self._expected_lights = []
+
     def on_solution_callback(self):
-        self.solutions.append([self.Value(x_mw) for mw, x_mw in self.variable_map.items()])
+        self.solutions.append([self.solver.Value(x_mw) for x_mw in self.variables])
 
     def by_entropy(self):
         solutions = np.array(self.solutions)
@@ -56,29 +72,47 @@ class SolutionScorerCallback(CpSolverSolutionCallback):
         entropies = np.log(probs)
         return entropies
 
-    def min_expected_matching(self) -> List[Couple_t]:
+    def scores(self):
+        if self._has_comp_distr:
+            return self._expected_n_sols
+        self._has_comp_distr = True
         sols = np.array(self.solutions)
         n_sols = len(self.solutions)
 
         # lights[i,j] gives us the number of lights sol. i gets if sol. j is perfect
         lights = np.dot(sols, sols.T)
+        self._expected_lights = np.mean(lights, axis=1)
 
         # gives us number of solutions in which i gets k lights
         n_sols_by_lights = np.apply_along_axis(np.bincount, 1, lights)
 
         # each sol's expected num lights
-        expected_n_sols = (n_sols_by_lights * n_sols_by_lights / n_sols).sum(axis=1)
+        self._expected_n_sols = (n_sols_by_lights * n_sols_by_lights / n_sols).sum(axis=1)
+        return self._expected_n_sols
 
+    def lights(self):
+        if self._has_comp_distr:
+            return self._expected_lights
+        self.scores()
+        return self._expected_lights
+
+    def min_expected_matching(self) -> List[Couple_t]:
         # best solutions is one which has the minimum number of expected solutions
-        argmin_exp = np.argmin(expected_n_sols)
+        scores = self.scores()
+        argmin_exp = np.argmin(scores)
         s = self.solutions[argmin_exp]
-        print(f"Optimal is matching {argmin_exp} with expected num sols remaining = {expected_n_sols[argmin_exp]}")
+        print(f"Optimal is matching {argmin_exp} with expected num sols remaining = {scores[argmin_exp]}")
         return [mw for i, mw in enumerate(self.keys) if s[i] == 1]
 
-    def evaluate_matching(self, matching: List[Couple_t]) -> float:
-        m = np.array(map(lambda mw: int(mw in matching), self.keys))
+    def evaluate_matching(self, matching: Matching_t) -> float:
+        m = np.array(list(map(lambda mw: int(mw in matching), self.keys)))
         n_sols = len(self.solutions)
         lights = np.dot(self.solutions, m)
         n_sols_by_lights = np.bincount(lights)
         expected_n_sols = np.dot(n_sols_by_lights, n_sols_by_lights / n_sols).sum()
         return expected_n_sols
+
+    def expected_lights(self, matching: Matching_t):
+        m = np.array(list(map(lambda mw: int(mw in matching), self.keys)))
+        lights = np.dot(self.solutions, m)
+        return np.mean(lights)
